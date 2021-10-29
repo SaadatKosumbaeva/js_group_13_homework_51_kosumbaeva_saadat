2eca1f32750e4794daa3f1b3c5ac6c3b8867175e
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2021-10-29 15:32:20 +0600

    Put together a "generator" app. Added code to html, css, ts files of "generator" component.

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-generator',
   templateUrl: './generator.component.html',
   styleUrls: ['./generator.component.css']
 })
-export class GeneratorComponent implements OnInit {
+export class GeneratorComponent {
+  numbers: number[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.numbers = this.generateNumbers();
   }
 
+  generateNumbers() {
+    this.numbers = [];
+    for (let i = 0; i < 5; i++) {
+      this.numbers.push(Math.floor(Math.random() * (36 - 5 + 1) + 5));
+    }
+    this.sortNumbers();
+    return this.numbers;
+  }
+
+  sortNumbers() {
+    this.numbers.sort(function (a, b) {
+      return a - b;
+    });
+  }
 }
