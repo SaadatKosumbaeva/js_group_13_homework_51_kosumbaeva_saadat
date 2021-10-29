2ade222ea2d542cb712e44f68529966030863879
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2021-10-29 15:35:15 +0600

    Created a component "image". Added code to html, css, ts files of this component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NumberComponent } from './number/number.component';
 import { GeneratorComponent } from './generator/generator.component';
+import { ImageComponent } from './image/image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NumberComponent,
-    GeneratorComponent
+    GeneratorComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule
