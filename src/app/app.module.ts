da0e4481f02fc5f9e71ec5d3fe1c644dacc6ec3e
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2021-10-29 15:36:30 +0600

    Created a component "gallery".

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NumberComponent } from './number/number.component';
 import { GeneratorComponent } from './generator/generator.component';
 import { ImageComponent } from './image/image.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NumberComponent,
     GeneratorComponent,
-    ImageComponent
+    ImageComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule
