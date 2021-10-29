c9df77724339b1f7b766ba0fec055bd87f8df60e
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2021-10-29 15:27:06 +0600

    Created a component "generator".

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NumberComponent } from './number/number.component';
+import { GeneratorComponent } from './generator/generator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NumberComponent
+    NumberComponent,
+    GeneratorComponent
   ],
   imports: [
     BrowserModule
